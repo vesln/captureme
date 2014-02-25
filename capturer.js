1009c7d21947246c2de6f9ad222f6d91f3ffaf46
Veselin Todorov <veselin@veselin.bg>
2014-02-25 10:11:42 +0200

    Extract Screenshot Capturer

---
+++ capturer.js
@@ -0,0 +1,64 @@
+/**
+ * Dependencies.
+ */
+
+var wd = require('wd');
+
+/**
+ * Screenshot capturer.
+ *
+ * @param {String} saucelabs user
+ * @param {String} saucelabs key
+ * @param {Object} browser config
+ * @constructor
+ */
+
+function Capturer(user, key, browser) {
+  this.user = user;
+  this.key = key;
+  this.browser = browser;
+}
+
+/**
+ * Capture a screenshot for `url`.
+ *
+ * @param {String} url
+ * @param {Function} fn
+ * @api public
+ */
+
+Capturer.prototype.capture = function(url, fn) {
+  var driver = this.driver();
+
+  var opts = {
+    browserName: this.browser.name,
+    platform: this.browser.platform,
+    version: this.browser.version
+  };
+
+  driver.init(opts, function(err) {
+    if (err) return fn(err);
+
+    driver.get(url, function(err) {
+      if (err) return fn(err);
+      driver.takeScreenshot(fn);
+    });
+  });
+};
+
+/**
+ * WebDriver remote factory.
+ *
+ * @returns {Object}
+ * @api private
+ */
+
+Capturer.prototype.driver = function() {
+  return wd.remote('ondemand.saucelabs.com', 80, this.user, this.key);
+};
+
+/**
+ * Primary export.
+ */
+
+module.exports = Capturer;
